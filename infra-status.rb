57636048032b59c89dd233b2b2d5db12bab947c7
Alex Legler <alex@a3li.li>
2013-11-12 10:14:11 +0100

    Set link in Atom feed

--- infra-status.rb
+++ infra-status.rb
@@ -40,9 +40,10 @@ end
 
 get '/feed.atom' do
   rss = RSS::Maker.make('atom') do |maker|
-    maker.channel.author = 'Gentoo Infrastructure Team'
-    maker.channel.title  = 'Gentoo Infrastructure Notices'
-    maker.channel.id     = MY_URL
+    maker.channel.author  = 'Gentoo Infrastructure Team'
+    maker.channel.title   = 'Gentoo Infrastructure Notices'
+    maker.channel.link    = MY_URL
+    maker.channel.id      = MY_URL
     maker.channel.updated = Time.now.to_s
 
     NoticeStore.instance.active_notices.each do |notice|
